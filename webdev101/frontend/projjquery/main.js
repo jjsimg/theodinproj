c67f392e1302ccd622f8a560a5c9d0e49783cded
Gallivan Student Networks <Gallivan Student Networks>
2017-06-19 12:39:59 -0600

    updates

--- webdev101/frontend/projjquery/main.js
+++ webdev101/frontend/projjquery/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+	$(".dropdown-button").dropdown();
+	gridsize=$('#board').css('width')/5;
+	console.log(gridsize);
 	
-
+	var grid=4;
+	for (i=0; i<grid; i++) {
+		$('#board1').append('<td class="boardtile"></td>')
+	}
 });
