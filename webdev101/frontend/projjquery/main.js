6f46070daabd386b24cc45af39c699727bc20d66
Gallivan Student Networks <Gallivan Student Networks>
2017-06-20 15:56:53 -0600

    updates

--- webdev101/frontend/projjquery/main.js
+++ webdev101/frontend/projjquery/main.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
-	$(".dropdown-button").dropdown();
+	$(".dropdown-button").dropdown({
+		belowOrigin: true
+	});
+	//Default grid size
+	grid=16;
+	//Initialize grid size
+	$('li').on('click', '#pickgrid', function(){
+		$(this).alert('woeijfweoifjW')
+	});
 	
-	//var grid=$('#gridsizepicker').val();;
-	var grid=80;
+	//Initialize Etch-a-Sketch
 	var ht=584/grid;
 	var wth=584/grid;
 	for (i=0; i<grid; i++) {
 		$('#board').append('<div class="boardtile" id="brow'+i+'"'+' style="height:'+ht+'px;width:'+wth+'px"></div>');
 		for (j=0; j<grid; j++) {
 			$('#brow'+i).append('<div class="boardtile" style="height:'+ht+'px;width:'+wth+'px"></div>')
-		}
-	}
+		};
+	};
+
+
+	//Change colours
 	$('#board').on('mouseover', '.boardtile', function(){
 		$(this).addClass("green");
 	});
