fe9022f63f09c4330db36c410fdb3c29212253a6
Steven Kim <crescent.spoon@gmail.com>
2017-06-19 19:01:16 -0600

    updates

--- webdev101/frontend/projjquery/main.js
+++ webdev101/frontend/projjquery/main.js
@@ -3,8 +3,13 @@ $(document).ready(function(){
 	gridsize=$('#board').css('width')/5;
 	console.log(gridsize);
 	
-	var grid=4;
+	var grid=20;
+	var ht=584/grid;
+	var wth=584/grid;
 	for (i=0; i<grid; i++) {
-		$('#board1').append('<td class="boardtile"></td>')
+		$('#board').append('<div class="boardtile" id="brow'+i+'"'+' style="height:'+ht+'px;width:'+wth+'px"></div>');
+		for (j=0; j<grid; j++) {
+			$('#brow'+i).append('<div class="boardtile" style="height:'+ht+'px;width:'+wth+'px"></div>')
+		}
 	}
 });
