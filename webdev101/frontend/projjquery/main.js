3722e126d9f93f8bb306e71fbe3afaeb8d73ed25
Steven Kim <crescent.spoon@gmail.com>
2017-06-20 20:28:55 -0600

    updates

--- webdev101/frontend/projjquery/main.js
+++ webdev101/frontend/projjquery/main.js
@@ -1,27 +1,35 @@
 $(document).ready(function(){
-	$(".dropdown-button").dropdown({
+	$('.dropdown-button').dropdown({
 		belowOrigin: true
 	});
-	//Default grid size
-	grid=16;
-	//Initialize grid size
-	$('li').on('click', '#pickgrid', function(){
-		$(this).alert('woeijfweoifjW')
+
+	$('.dropdown-content').on('click', function(){
+		$('ul').on('click', 'li', function(){
+			var idcolor=$(this).attr('id');
+			$('#board').on('mouseover', '.boardtile', function(){
+				$(this).attr('id', idcolor);
+			});
+		});
 	});
-	
-	//Initialize Etch-a-Sketch
-	var ht=584/grid;
-	var wth=584/grid;
-	for (i=0; i<grid; i++) {
-		$('#board').append('<div class="boardtile" id="brow'+i+'"'+' style="height:'+ht+'px;width:'+wth+'px"></div>');
-		for (j=0; j<grid; j++) {
-			$('#brow'+i).append('<div class="boardtile" style="height:'+ht+'px;width:'+wth+'px"></div>')
+
+	$('#pickgrid').on('click', function(){
+		//Check if board is empty
+		var grid=prompt('Choose a grid size between 16 and 100')
+		if ($('#outer').find('#board').length!==0) {
+			var blength=$('#outer').find('#board').length;
+			$('#board').remove()
+		}
+		//Maintain a gridsize between 16x16 and 100x100
+		if (grid>=16 && grid<=100) {
+			var ht=584/grid;
+			var wth=584/grid;
+			$('#outer').append('<div id="board"></div>')
+			for (i=0; i<grid; i++) {
+				$('#board').append('<div class="boardtile" id="brow'+i+'"'+' style="height:'+ht+'px;width:'+wth+'px"></div>');
+				for (j=0; j<grid; j++) {
+					$('#brow'+i).append('<div class="boardtile" style="height:'+ht+'px;width:'+wth+'px"></div>');
+				};
+			};
 		};
-	};
-
-
-	//Change colours
-	$('#board').on('mouseover', '.boardtile', function(){
-		$(this).addClass("green");
 	});
 });
