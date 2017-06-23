b5c5b6cc179c658fedb7230e291c3980a431713d
Gallivan Student Networks <Gallivan Student Networks>
2017-06-23 15:49:12 -0600

    updates

--- webdev101/backend/projlearnruby/01_temperature/temperature.rb
+++ webdev101/backend/projlearnruby/01_temperature/temperature.rb
@@ -1,1 +1,12 @@
 #write your code here
+def ftoc(temp)
+	temp=Float(temp)
+	temp=(temp-Float(32))*Float(5)/Float(9)
+	Integer(temp)==temp ? temp=Integer(temp) : temp=Float("%.1f" % temp)
+end
+
+def ctof(temp)
+	temp=Float(temp)
+	temp=temp*(Float(9)/Float(5))+32
+	Integer(temp)==temp ? temp=Integer(temp) : temp=Float("%.1f" % temp)
+end
