b29cc939b74441614b3d96ca31f2e899e06dbe52
Gallivan Student Networks <Gallivan Student Networks>
2017-06-24 16:07:04 -0600

    updates

--- webdev101/backend/projlearnruby/04_pig_latin/pig_latin.rb
+++ webdev101/backend/projlearnruby/04_pig_latin/pig_latin.rb
@@ -1,1 +1,8 @@
 #write your code here
+def translate(phrase)
+	vow="aeiou".split("")
+	newphrase=""
+	phrase.split.each {|word| (vow.include? word[0]) ? newphrase+=word+"ay" : newphrase+=word[1..word.length]+word[0]+"ay"}
+	phrase.split.each {|word| word.each {|letter|}}
+	newphrase
+end
