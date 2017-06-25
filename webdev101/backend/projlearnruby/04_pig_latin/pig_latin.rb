33b8de8b01ccb336228ef7fa92baa1b0c305a6fc
Gallivan Student Networks <Gallivan Student Networks>
2017-06-25 14:03:34 -0600

    updates

--- webdev101/backend/projlearnruby/04_pig_latin/pig_latin.rb
+++ webdev101/backend/projlearnruby/04_pig_latin/pig_latin.rb
@@ -4,9 +4,14 @@ def translate(phrase)
 	newphrase=""
 	phrase.split.each do |word|
 		cons=""
-		word.split("").each_with_index do |letter,index| 
+		word.split("").each_with_index do |letter,index|
+			index==0 and letter==letter.upcase ? capword=true : capword=false
+
 			if letter=="u" and index>0 or not vow.include? letter
 				cons+=letter
+			elsif vow.include? letter and capword #why is it not evaluating this conditional as true?
+				newphrase+=word[index..word.length].downcase.capitalize+cons+"ay"+" "
+				break
 			elsif vow.include? letter
 				newphrase+=word[index..word.length]+cons+"ay"+" "
 				break
