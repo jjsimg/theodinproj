fd38c36d1830fdc1e5fb0ab35b3c7849e06c7e44
Gallivan Student Networks <Gallivan Student Networks>
2017-06-25 15:31:49 -0600

    updates

--- webdev101/backend/projlearnruby/06_timer/timer.rb
+++ webdev101/backend/projlearnruby/06_timer/timer.rb
@@ -1,3 +1,13 @@
 class Timer
   #write your code here
+  attr_reader :seconds
+  def initialize
+  	@seconds=0
+  end
+  def time_string
+  	@seconds
+  end
 end
+@timer=Timer.new
+# @timer.seconds=4
+puts @timer.seconds
