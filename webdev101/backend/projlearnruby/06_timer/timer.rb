df1e7882c41ca00e2a4848c13151974c8d58eaf3
Steven Kim <crescent.spoon@gmail.com>
2017-06-25 18:19:34 -0600

    updates

--- webdev101/backend/projlearnruby/06_timer/timer.rb
+++ webdev101/backend/projlearnruby/06_timer/timer.rb
@@ -1,13 +1,14 @@
 class Timer
-  #write your code here
-  attr_reader :seconds
-  def initialize
-  	@seconds=0
-  end
-  def time_string
-  	@seconds
-  end
+	#write your code here
+	attr_accessor :seconds
+
+	def initialize
+		@seconds=0
+	end
+	def time_string
+		@hours=(@seconds/(60*60))%60
+		@minutes=(@seconds/60)%60
+		@seconds=@seconds%60
+		time="%02d:%02d:%02d" % [@hours, @minutes, @seconds]
+	end
 end
-@timer=Timer.new
-# @timer.seconds=4
-puts @timer.seconds
