eea527df8505a1a8ff1caf57790eade7572ca0f6
Gallivan Student Networks <Gallivan Student Networks>
2017-07-05 15:08:29 -0600

    updates

--- 02_rubyprog/basicruby/oop/tictactoe.rb
+++ 02_rubyprog/basicruby/oop/tictactoe.rb
@@ -1,69 +1,61 @@
-# class Choice
-# 	def initialize(p1move)
-# 		@@p1move=p1move
-# 	end
-# end
-
-# class TicTacToe < Choice
-# 	p1move=@@p1move
-# 	def board
-# 		board=[1,2,3,
-# 					 4,5,6,
-# 					 7,8,9]
-# 	end
-
-# 	def show
-# 		puts "#{board[0]}"+"|"+"#{board[1]}"+"|"+"#{board[2]}"
-# 		puts "-"+"-"+"-"+"-"+"-"
-# 		puts "#{board[3]}"+"|"+"#{board[4]}"+"|"+"#{board[5]}"
-# 		puts "-"+"-"+"-"+"-"+"-"
-# 		puts "#{board[6]}"+"|"+"#{board[7]}"+"|"+"#{board[8]}"
-# 	end
-
-# 	def self.win?
-
-# 	end
-
-# 	puts "Are you ready to play Tic Tac Toe?"
-# 	puts "Player 1, where would you like to go?"
-
-# 	if @@p1move<=9 and @@p1move>=1 and board[@@p1move]!='x'
-# 		board[@p1move]=x
-# 	end
-# end
-
-
-# game1=TicTacToe.new
-# game1.show
 class TicTacToe
 
-	@@board=[1,2,3,
-			     4,5,6,
-			     7,8,9]
+	@@board=[0,1,2,
+			     3,4,5,
+			     6,7,8]
 
 	def self.show
+		puts ""
+		puts ""
 		puts "#{@@board[0]}"+"|"+"#{@@board[1]}"+"|"+"#{@@board[2]}"
 		puts "-"+"-"+"-"+"-"+"-"
 		puts "#{@@board[3]}"+"|"+"#{@@board[4]}"+"|"+"#{@@board[5]}"
 		puts "-"+"-"+"-"+"-"+"-"
 		puts "#{@@board[6]}"+"|"+"#{@@board[7]}"+"|"+"#{@@board[8]}"
+		puts ""
+		puts ""
 	end
 
-	puts "Let's play Tic Tac Toe."
-	show
-
-	puts "Player 1, you are x."
-	puts "Give me your choice of where to go: "
-	p1move=Integer(gets.chomp)
-	while p1move>9 or p1move<1
-		print "Please choose again: "
-		p1move=Integer(gets.chomp)
+	def self.welcome
+		puts ""
+		puts ""
+		puts ""
+		puts "Let's play Tic Tac Toe."
+		show
+		puts "Player 1, you are x."
+		puts ""
+		puts "Player 2, you are o."
+		puts ""
+		puts ""
 	end
 
+	def self.game_over
 
-	@@board[p1move-1]="x"
+	end
+	# Loop through this
+	def self.choice
+		p1="x"
+		p2="o"
+		xcount=@@board.count("x")
+		ocount=@@board.count("o")
+		print "Give me your choice of where to go: "
+		move=gets.chomp.to_i
+		@@board[move]="o" if xcount>ocount
+		@@board[move]="x" if xcount==ocount
+	end
+	
+	welcome
+	# until game_over
+	choice
 	show
+	choice
+	show
+	choice
+	show
+	choice
+	show
+	# end
+	
 end
 
 a=TicTacToe.new
-a
