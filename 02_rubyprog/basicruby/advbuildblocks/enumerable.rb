6e00bda14929d2f0e6c84b4af3d6fdd7ea5ac919
Steven Kim <crescent.spoon@gmail.com>
2017-07-04 19:58:50 -0600

    updates

--- 02_rubyprog/basicruby/advbuildblocks/enumerable.rb
+++ 02_rubyprog/basicruby/advbuildblocks/enumerable.rb
@@ -1,100 +1,100 @@
 module Enumerable
-	def my_each
-	    for i in 0...self.length
-	    	yield(self[i])
-	    end
-	end
+  def my_each
+      for i in 0...self.length
+        yield(self[i])
+      end
+  end
 
-	def my_each_with_index
-		for i in 0...self.length
-			yield(self[i],i)
-		end
-	end
+  def my_each_with_index
+    for i in 0...self.length
+      yield(self[i],i)
+    end
+  end
 
-	def my_select
-		result=[]
-		for i in 0...self.length
-			if yield(self[i])
-				result.push(self[i])
-			end
-		end
-		result
-	end
+  def my_select
+    result=[]
+    for i in 0...self.length
+      if yield(self[i])
+        result.push(self[i])
+      end
+    end
+    result
+  end
 
-	def my_all?
-		for i in 0...self.length
-			if yield(self[i])==false
-				return false
-			end
-		end
-		true
-	end
+  def my_all?
+    for i in 0...self.length
+      if yield(self[i])==false
+        return false
+      end
+    end
+    true
+  end
 
-	def my_any?
-		for i in 0...self.length
-			if yield(self[i])
-				return true
-			end
-		end
-		false
-	end
+  def my_any?
+    for i in 0...self.length
+      if yield(self[i])
+        return true
+      end
+    end
+    false
+  end
 
-	def my_none?
-		for i in 0...self.length
-			if yield(self[i])
-				return false
-			end
-		end
-		true
-	end
+  def my_none?
+    for i in 0...self.length
+      if yield(self[i])
+        return false
+      end
+    end
+    true
+  end
 
-	def my_count(input=nil)
-		if block_given?
-			count=0
-			for i in 0...self.length
-				if yield(self[i])
-					count+=1
-				end
-			end
-			count
-		elsif input.is_a? Integer
-			count=0
-			for i in 0...self.length
-				if self[i]==input
-					count+=1
-				end
-			end
-			count
-		elsif input==nil
-			self.length
-		end
-	end
+  def my_count(input=nil)
+    if block_given?
+      count=0
+      for i in 0...self.length
+        if yield(self[i])
+          count+=1
+        end
+      end
+      count
+    elsif input.is_a? Integer
+      count=0
+      for i in 0...self.length
+        if self[i]==input
+          count+=1
+        end
+      end
+      count
+    elsif input==nil
+      self.length
+    end
+  end
 
-	def my_inject(input=nil)
-		if input==nil
-			accum=yield(self[0],self[1])
-		else
-			accum=yield(input,yield(self[0],self[1]))
-		end
-		for i in 1...self.length-1
-			accum=yield(accum,self[i+1]) 
-		end
-		accum
-	end
+  def my_inject(input=nil)
+    if input==nil
+      accum=yield(self[0],self[1])
+    else
+      accum=yield(input,yield(self[0],self[1]))
+    end
+    for i in 1...self.length-1
+      accum=yield(accum,self[i+1]) 
+    end
+    accum
+  end
 
-	def multiply_els
-		# must use #my_inject method
-		self.my_inject do |product,x|
-			product*x
-		end
-	end
+  def multiply_els
+    # must use #my_inject method
+    self.my_inject do |product,x|
+      product*x
+    end
+  end
 
-	def my_map
-		# must take block and proc
-		result=[]
-		for i in 0...self.length
-			result.push(yield(self[i]))
-		end
-		result
-	end
+  def my_map
+    # must take block and proc
+    result=[]
+    for i in 0...self.length
+      result.push(yield(self[i]))
+    end
+    result
+  end
 end
--- 02_rubyprog/basicruby/advbuildblocks/testscript.rb
+++ 02_rubyprog/basicruby/advbuildblocks/testscript.rb
@@ -1,11 +1,11 @@
 require "./enumerable"
 
 class My_methods
-	include Enumerable
-	attr_reader :items
-	def initialize(items)
-		@items=items
-	end
+  include Enumerable
+  attr_reader :items
+  def initialize(items)
+    @items=items
+  end
 end
 
 array1=[-2,5,7,9,3,6,1]
@@ -13,11 +13,11 @@ hash1={'item1':'horse', 'item2':'giraffe', 'item3':'monkey'}
 a=My_methods.new(array1)
 
 # d=Proc.new do |x|
-# 	x+2
+#   x+2
 # end
 
 # f=lambda do |x|
-# 	x+3
+#   x+3
 # end
 
 # c=a.items.inject
@@ -25,12 +25,12 @@ a=My_methods.new(array1)
 # print c
 
 c=a.items.inject do |product,n| 
-	product*n
+  product*n
 end
 print c
 
 b=a.items.my_inject do |product,n| 
-	product*n
+  product*n
 end
 print b
 
