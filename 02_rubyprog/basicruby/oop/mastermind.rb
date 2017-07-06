24f5dc6a938f2207634e172eb63d3281bde665e3
Gallivan Student Networks <Gallivan Student Networks>
2017-07-06 15:16:14 -0600

    updates

--- 02_rubyprog/basicruby/oop/mastermind.rb
+++ 02_rubyprog/basicruby/oop/mastermind.rb
@@ -14,37 +14,74 @@ class Mastermind
     puts ""
   end
 
+    # puts ""
+    # puts "    "+"X-X-X-X-X----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts "    "+"o-o-o-o-o----------"
+    # puts ""
+
   def self.gameboard
     puts ""
-    puts "XXXXX--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
-    puts "ooooo--------"
+    puts "    "+"X-X-X-X-X----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"#{@@mychoice[0]}-#{@@mychoice[1]}-#{@@mychoice[2]}-#{@@mychoice[3]}-#{@@mychoice[4]}----------"
     puts ""
   end
 
-  def self.randomize
-    blue=1
-    red=2
-    white=3
-    violet=4
-    orange=5
-    green=6
-    yellow=7
-    @@mastermind=[]
-    (1..5).each do |x|
-      @@mastermind.push(rand(1..7))
+
+  def self.initgameboard
+    @@mychoice=['o','o','o','o','o']
+    @@check=[]
+    if @@mychoice[0]==@@mastermind[0]
+      @@check.push(2)
+    elsif @@mychoice.count(1..5.each { |x| @@mastermind[x] })
+      count+=1
+    elsif
+    line=[]
+    for i in 0...10
+      line[i].push(@@mychoice)
     end
-    # this doesn't do what I want yet
-    (0...5).map { ('a'..'z').to_a[rand(26)] }
+    puts ""
+    puts "    "+"X-X-X-X-X----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    puts "    "+"o-o-o-o-o----------"
+    line4= "    "+"o-o-o-o-o----------"
+    line3= "    "+"o-o-o-o-o----------"
+    line2= "    "+"o-o-o-o-o----------"
+    line1= "    "+"#{@@mychoice[0]}-#{@@mychoice[1]}-#{@@mychoice[2]}-#{@@mychoice[3]}-#{@@mychoice[4]}----------"
+    puts ""
+  end
+    line[i]="    "+"#{@@mychoice[0]}-#{@@mychoice[1]}-#{@@mychoice[2]}-#{@@mychoice[3]}-#{@@mychoice[4]}----------"
+
+    if @@mychoice.count(@@mastermind)
+
+  def self.randomize
+    # blue red white violet orange green yellow
+    clrarray=['b','r','w','v','o','g','y']
+    @@mastermind=[]
+    (1..5).each do
+      @@mastermind.push(clrarray[rand(0..6)])
+    end
   end
 
   def self.choice
@@ -59,7 +96,10 @@ class Mastermind
     puts "(y)ellow"
     puts "-----------------------------------------------"
     puts "Make sure to select letters separated by spaces"
-    mychoice=gets.split
+    @@mychoice=gets.split
+    if game_over
+      #do something
+    end
   end
 
   welcome
