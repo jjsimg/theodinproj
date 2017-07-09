3e00a49b9509bf031d4e8a80d443881cc152d96f
Gallivan Student Networks <Gallivan Student Networks>
2017-07-09 15:26:06 -0600

    updates

--- 02_rubyprog/basicruby/fileIO/event_manager/lib/event_manager.rb
+++ 02_rubyprog/basicruby/fileIO/event_manager/lib/event_manager.rb
@@ -91,9 +91,81 @@
 #   puts "#{name} #{zipcode} #{legislator_string}"
 # end
 
-# Moving Displaying Legislators to a Method
+# Moving Displaying Legislators to a Method and using a Form Letter
+# require "csv"
+# require 'sunlight/congress'
+
+# Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+# def clean_zipcode(zipcode)
+#   zipcode.to_s.rjust(5,"0")[0..4]
+# end
+
+# def legislators_by_zipcode(zipcode)
+#   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+#   legislator_names = legislators.collect do |legislator|
+#     "#{legislator.first_name} #{legislator.last_name}"
+#   end
+#   legislator_names.join(", ")
+# end
+
+# template_letter = File.read "form_letter.html"
+
+# contents=CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   name = row[:first_name] # We can access column names with symbols
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislators_by_zipcode(zipcode)
+
+#   personal_letter = template_letter.gsub('FIRST_NAME',name)
+#   personal_letter = personal_letter.gsub('LEGISLATORS',legislators)
+
+#   puts personal_letter
+# end
+
+# ERB
+# require 'erb'
+
+# meaning_of_life = 42
+
+# question = "The Answer to the Ultimate Questions of Life, the Universe, and Everything is <%= meaning_of_life %>"
+# template = ERB.new question
+
+# results = template.result(binding)
+# puts results
+
+# Moving Displaying Legislators to a Method and using a Form Letter
+# require "csv"
+# require 'sunlight/congress'
+# require 'erb'
+
+# Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+# def clean_zipcode(zipcode)
+#   zipcode.to_s.rjust(5,"0")[0..4]
+# end
+
+# def legislators_by_zipcode(zipcode)
+#   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+# end
+
+# template_letter = File.read "form_letter.erb"
+# erb_template = ERB.new template_letter
+
+# contents=CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   name = row[:first_name] # We can access column names with symbols
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislators_by_zipcode(zipcode)
+
+#   form_letter = erb_template.result(binding)
+#   puts form_letter
+# end
+
+# Outputting form letters to a file and put it in a method called save_thank_you_letter
 require "csv"
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -103,17 +175,26 @@ end
 
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-  legislator_names.join(", ")
 end
+
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+  filename = "output/thanks_#{id}.html"
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents=CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
+  id = row[0]
   name = row[:first_name] # We can access column names with symbols
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  save_thank_you_letters(id,form_letter)
 end
