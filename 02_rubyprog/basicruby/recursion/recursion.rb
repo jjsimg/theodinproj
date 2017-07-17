509d186ab4d4e099e9605e84aa1b61d08defb380
Steven Kim <crescent.spoon@gmail.com>
2017-07-16 20:41:45 -0600

    updates

--- 02_rubyprog/basicruby/recursion/recursion.rb
+++ 02_rubyprog/basicruby/recursion/recursion.rb
@@ -1,13 +1,22 @@
-def merge_sort(array)
-	# while sorted_array != array.sort
-	sorted_array = Array.new
-		array.each_with_index do |x, idx|
-			if idx%2==0
-				sorted_array.push(array.select { array[idx] < array[idx+1] })
-			end
-		end
-		print sorted_array
-	# end
+def merge_sort(list)
+  return list if list.size <= 1
+  mid   = list.size / 2
+  left  = list[0, mid]
+  right = list[mid, list.size]
+  merge(merge_sort(left), merge_sort(right))
 end
 
-merge_sort([4,3,2,8,13,2,9,7])
+def merge(left, right)
+  sorted = []
+  until left.empty? or right.empty?
+    if left.first <= right.first
+      sorted << left.shift
+    else
+      sorted << right.shift
+    end
+  end
+  sorted.concat(left).concat(right)
+end
+
+
+print merge_sort([4,3,2,8,13,2,9,7])
