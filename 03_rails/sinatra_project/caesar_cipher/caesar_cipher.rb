f19f31f748bcdaf81eac70166495aedccbd8c5e8
jjsimg <crescent.spoon@gmail.com>
2017-08-10 13:00:36 -0600

    updates

--- 03_rails/sinatra_project/caesar_cipher/caesar_cipher.rb
+++ 03_rails/sinatra_project/caesar_cipher/caesar_cipher.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+
+
 
 get '/' do
-  "Hello World! oiwejfoewjfie"
+	guess = params["guess"]
+	message = check_guess(guess)
+	colour = get_colour(guess)
+	erb :index, :locals => { :random_num => @@random_num, :message => message, :colour => colour }
 end
