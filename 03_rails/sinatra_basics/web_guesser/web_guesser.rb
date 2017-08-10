55adb731f70307865ed4768ca42b5b09dab231d6
Steven Kim <crescent.spoon@gmail.com>
2017-08-09 20:15:24 -0600

    updates

--- 03_rails/sinatra_basics/web_guesser/web_guesser.rb
+++ 03_rails/sinatra_basics/web_guesser/web_guesser.rb
@@ -1,28 +1,48 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_num = rand(101)
+RANDOM_NUM = rand(101)
 
-def check_guess(guess, random_num)
+def initialize
+	@@guesses_remain = 5
+end
+
+def check_guess(guess)
 	guess = guess.to_i
-	if guess == ""
-		"Enter an integer!"
-	elsif guess.between?(random_num-5, random_num+5) and guess < random_num
-		"Too Low!"
-	elsif guess.between?(random_num-5, random_num+5) and guess > random_num
-		"Too High!"
-	elsif guess < random_num
+	if guess == 0
+		"Not a number! Enter an Integer!"
+	elsif guess.between?(RANDOM_NUM-5, RANDOM_NUM+5) and guess < RANDOM_NUM
+		"Low! But you're close!"
+	elsif guess.between?(RANDOM_NUM-5, RANDOM_NUM+5) and guess > RANDOM_NUM
+		"High! But you're close"
+	elsif guess < RANDOM_NUM
+		@@guesses_remain += -1
 		"Way Too Low!"
-	elsif guess > random_num
+	elsif guess > RANDOM_NUM
+		@@guesses_remain += -1
 		"Way Too High!"
-	elsif guess == random_num
+	elsif guess == RANDOM_NUM
 		"Correct!"
 	end
 end
 
+def get_colour(guess)
+	guess = guess.to_i
+	return "green" if guess == RANDOM_NUM
+	return "indianred" if guess.between?(RANDOM_NUM-5, RANDOM_NUM+5)
+	return "red" if guess < RANDOM_NUM or guess > RANDOM_NUM
+end
+
+def check_num_guesses
+	if @@guesses == 0
+		"No more guesses left"
+	end
+end
 
 get '/' do
 	guess = params["guess"]
-	message = check_guess(guess, random_num)
-	erb :index, :locals => { :random_num => random_num, :message => message }
+	message = check_guess(guess)
+	colour = get_colour(guess)
+	message = check_num_guesses
+	erb :index, :locals => { :random_num => RANDOM_NUM, :message => message, :colour => colour }
 end
