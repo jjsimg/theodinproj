614b7eb8967e24d1db0daab912f4f1feec3a16a3
jjsimg <crescent.spoon@gmail.com>
2017-08-10 12:13:37 -0600

    updates

--- 03_rails/sinatra_basics/web_guesser/web_guesser.rb
+++ 03_rails/sinatra_basics/web_guesser/web_guesser.rb
@@ -1,47 +1,52 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-RANDOM_NUM = rand(101)
+@@random_num = rand(101)
+@@guesses_remain = 30
+@@game_over = false
 
-@@guesses_remain = 5
-
-
-def check_guess(guess)
+def check_guess(guess) 
+	if @@game_over
+		@@random_num = rand(101)
+		@@game_over = false
+	end
 	guess = guess.to_i
+	if @@guesses_remain == 0
+		@@guesses_remain = 30
+		@@game_over = true
+		return "You have run out of guesses!"
+	end
 	if guess == 0
 		"Not a number! Enter an Integer!"
-	elsif guess.between?(RANDOM_NUM-5, RANDOM_NUM+5) and guess < RANDOM_NUM
-		"Low! But you're close!"
-	elsif guess.between?(RANDOM_NUM-5, RANDOM_NUM+5) and guess > RANDOM_NUM
-		"High! But you're close"
-	elsif guess < RANDOM_NUM
+	elsif guess.between?(@@random_num-5, @@random_num+5) and guess < @@random_num
 		@@guesses_remain += -1
-		"Way Too Low!"
-	elsif guess > RANDOM_NUM
+		"Low! But you're close! You have #{@@guesses_remain} guesses remaining"
+	elsif guess.between?(@@random_num-5, @@random_num+5) and guess > @@random_num
 		@@guesses_remain += -1
-		"Way Too High!"
-	elsif guess == RANDOM_NUM
+		"High! But you're close! You have #{@@guesses_remain} guesses remaining"
+	elsif guess < @@random_num
+		@@guesses_remain += -1
+		"Way Too Low! You have #{@@guesses_remain} guesses remaining"
+	elsif guess > @@random_num
+		@@guesses_remain += -1
+		"Way Too High! You have #{@@guesses_remain} guesses remaining"
+	elsif guess == @@random_num
+		@@guesses_remain = 30
+		@@gamve_over = true
 		"Correct!"
 	end
 end
 
 def get_colour(guess)
 	guess = guess.to_i
-	return "green" if guess == RANDOM_NUM
-	return "indianred" if guess.between?(RANDOM_NUM-5, RANDOM_NUM+5)
-	return "red" if guess < RANDOM_NUM or guess > RANDOM_NUM
-end
-
-def check_num_guesses
-	if @@guesses_remain == 0
-		"No more guesses left"
-	end
+	return "green" if guess == @@random_num
+	return "indianred" if guess.between?(@@random_num-5, @@random_num+5)
+	return "red" if guess < @@random_num or guess > @@random_num
 end
 
 get '/' do
 	guess = params["guess"]
 	message = check_guess(guess)
 	colour = get_colour(guess)
-	message = check_num_guesses
-	erb :index, :locals => { :random_num => RANDOM_NUM, :message => message, :colour => colour }
+	erb :index, :locals => { :random_num => @@random_num, :message => message, :colour => colour }
 end
