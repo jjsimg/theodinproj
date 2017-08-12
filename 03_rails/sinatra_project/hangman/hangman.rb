1fd991ea8dd2b49fcd4e2c24263d4e3ead991811
jjsimg <crescent.spoon@gmail.com>
2017-08-12 11:01:44 -0600

    updates

--- 03_rails/sinatra_project/hangman/hangman.rb
+++ 03_rails/sinatra_project/hangman/hangman.rb
@@ -8,15 +8,23 @@ class Hangman
 		@dictionary = File.open('sample_dictionary.txt','r')
 	end
 
+	def get_word
+		File.readlines('sample_dictionary.txt').sample
+	end
+
 	def play
 		"You are playing Hangman"
 	end
 end
 
 game = Hangman.new
-dict = game.dictionary
+
+
+enable :sessions
 
 get '/' do
-	secret_message = game.dictionary
+	# secret_message = game.get_word
+	@session = session
+	secret_message = @session
 	erb :index, :locals => { :secret_message => secret_message }
 end
