792433d978c9e0de743f489981ff6b7a4489ae23
Steven Kim <crescent.spoon@gmail.com>
2017-08-11 21:00:33 -0600

    updates

--- 03_rails/sinatra_project/hangman/hangman.rb
+++ 03_rails/sinatra_project/hangman/hangman.rb
@@ -2,14 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 class Hangman
+	attr_reader :dictionary
+
+	def initialize
+		@dictionary = File.open('sample_dictionary.txt','r')
+	end
+
 	def play
 		"You are playing Hangman"
 	end
 end
 
 game = Hangman.new
+dict = game.dictionary
 
 get '/' do
-	secret_message = game.play
+	secret_message = game.dictionary
 	erb :index, :locals => { :secret_message => secret_message }
 end
